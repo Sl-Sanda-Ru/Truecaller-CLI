2dee108ff642af574668a46c847a2e32b2d3e0d8
Sandaru Ashen Fernando <68476573+Sl-Sanda-Ru@users.noreply.github.com>
2021-09-22 13:12:49 +0530

    Update truecaller.py

--- truecaller.py
+++ truecaller.py
@@ -62,7 +62,7 @@ def authveri(id, num, pin):
         return req.json()['installationId']
 
 def numsearch(authkey,num):
-    params = {'q':num,'countryCode':'','type':'4','locAddr':'','placement':'SEARCHRESULTS,HISTORY,DETAILS','encoding':'json'}
+    params = {'q':num, 'countryCode':'', 'type':'4', 'locAddr':'', 'placement':'SEARCHRESULTS,HISTORY,DETAILS', 'encoding':'json'}
     headers = {'content-type':'application/json; charset=UTF-8', 'accept-encoding':'gzip', 'user-agent':'Truecaller/11.75.5 (Android;10)', 'clientsecret':'lvc22mp3l1sfv6ujg83rd17btt', 'authorization':'Bearer ' + authkey}
     req = requests.get('https://search5-noneu.truecaller.com/v2/search', headers=headers, params=params)
     if req.json().get('status'):
